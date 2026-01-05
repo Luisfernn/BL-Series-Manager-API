12434e21a5606696b3612be87c152098ffbe556c
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-05 16:30:07 -0300

    Criando o script de tags.py.

---
+++ app/models/tags.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.models.base import Base
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
