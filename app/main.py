31ed74f0ffbd8213e8a06ea632644c27b98cae1d
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-05 17:40:05 -0300

    Referenciando routes em main.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.api.routes.series import router as series_router
+
 
 app = FastAPI()
+
+app.include_router(series_router)
+
 
 @app.get("/health")
 def health():
