847bd8e4b1f8380e179b9a6f70489c91444295cf
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-23 19:34:56 -0300

    Criando script do model series_characters.

---
+++ app/models/series_characters.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+
+
+class SeriesCharacter(Base):
+    __tablename__ = "series_characters"
+
+    id = Column(Integer, primary_key=True)
+
+    series_id = Column(Integer, ForeignKey("series.id"), nullable=False)
+    actor_id = Column(Integer, ForeignKey("actors.id"), nullable=False)
+
+    character_name = Column(String, nullable=False)
+
+    series = relationship("Series", back_populates="characters")
+    actor = relationship("Actor", back_populates="characters")
