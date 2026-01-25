d96b985bbd4ed7c7dcdceb3c92118a707b5203aa
Luis Fernando <luisfernandowubi@gmail.com>
2026-01-25 19:37:43 -0300

    Importando todos os models para regristrar eles com sqlalchemy.

---
+++ app/models/__init__.py
@@ -0,0 +1,34 @@
+# Import all models to register them with SQLAlchemy
+from app.models.base import Base
+from app.models.actors import Actor
+from app.models.series import Series
+from app.models.characters import Character
+from app.models.tags import Tag
+from app.models.ship_actors import ShipActor
+from app.models.ship_characters import ShipCharacter
+
+# Association tables
+from app.models.series_actors import SeriesActor
+from app.models.series_tag import SeriesTag
+from app.models.series_characters import SeriesCharacter
+from app.models.ship_actors_actors import ShipActorActor
+from app.models.ship_actors_series import ShipActorSeries
+from app.models.ship_characters_characters import ShipCharacterCharacter
+from app.models.ship_characters_series import ShipCharacterSeries
+
+__all__ = [
+    "Base",
+    "Actor",
+    "Series",
+    "Character",
+    "Tag",
+    "ShipActor",
+    "ShipCharacter",
+    "SeriesActor",
+    "SeriesTag",
+    "SeriesCharacter",
+    "ShipActorActor",
+    "ShipActorSeries",
+    "ShipCharacterCharacter",
+    "ShipCharacterSeries",
+]
